768584e080ab1108385b63623478466a2188de2b
hardingcheng <hardingcheng@163.com>
2022-02-23 19:07:52 +0800

    刷图

--- temp.js
+++ temp.js
@@ -1,20 +1,40 @@
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
-var isStraight = function (nums) {
-  let set = new Set();
-  let max = 0,
-    min = 14;
-  for (let num of nums) {
-    // 跳过大小王;
-    if (num === 0) continue;
-    max = Math.max(num, max);
-    min = Math.min(num, min);
-    // 若有重复，提前返回 false
-    if (set.has(num)) return false;
-    else set.add(num);
+// 找到最大数的下标
+function getMaxIndex(nums) {
+  let max = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] > nums[max]) {
+      max = i;
+    }
   }
-  // 最大牌 - 最小牌 < 5 则可构成顺子
-  return max - min < 5;
+  return max;
+}
+// 反转前k个元素
+var reverse = function (arr, k) {
+  if (k < 1) {
+    return;
+  }
+  let i = 0,
+    j = k;
+  while (i < j) {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+    i++;
+    j--;
+  }
+};
+var pancakeSort = function (arr) {
+  let ans = [],
+    max;
+  while (arr.length > 0) {
+    max = getMaxIndex(arr);
+    // ans 存储反转的长度
+    if (max === arr.length - 1) {
+      continue;,
+    }
+    max > 0 && ans.push(max + 1);
+    reverse(arr, max);
+    reverse(arr, arr.length - 1);
+    ans.push(arr.length);
+    arr.pop();
+  }
+  return ans;
 };
